eb31789486431da75ffcb357ae40edca442bed67
Suyeon Cha <suyeonc@andrew.cmu.edu>
2025-03-19 14:45:54 -0400

    models + create playlist

--- testing_container/spotifytest/models.py
+++ testing_container/spotifytest/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.utils import timezone
 import uuid
 
@@ -11,6 +11,25 @@ class User(AbstractUser):
     spotify_token = models.CharField(max_length=255, blank=True, null=True)
     friends = models.ManyToManyField('self', symmetrical=True, blank=True)
     liked_playlists = models.ManyToManyField('Playlist', related_name='liked_by', blank=True)
+    
+    # Fix the clash by adding custom related_name parameters
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name='spotifytest_user_set',  # Custom related_name
+        related_query_name='user',
+    )
+    
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='spotifytest_user_set',  # Custom related_name
+        related_query_name='user',
+    )
 
     def __str__(self):
         return self.username
