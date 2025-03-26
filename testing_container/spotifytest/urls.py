ad27dc5aad6672297af98bfe15e80a632b7af2e5
Suyeon Cha <suyeonc@andrew.cmu.edu>
2025-03-26 16:55:56 -0400

    fix urls

--- testing_container/spotifytest/urls.py
+++ testing_container/spotifytest/urls.py
@@ -3,10 +3,10 @@ from spotifytest import views
 
 urlpatterns = [
     path('', views.spotify_login, name='spotify-login'),
-    path('user-profile', views.user_profile, name='user-profile'),
-    path('playlist-complete', views.playlist_complete, name='playlist-complete'),
+    path('user-profile/', views.user_profile, name='user-profile'),
+    path('playlist-complete/', views.playlist_complete, name='playlist-complete'),
     # path('', views.track_list, name='track-list'),
-    path('home', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('callback/', views.spotify_callback, name='spotify-callback'),
     path("create_spotify_playlist/", views.create_spotify_playlist, name="create_spotify_playlist"),
 ]
